c55067eedc65327ebf5794ccaea6c8f386f6ee79
ethandudu <41381482+ethandudu@users.noreply.github.com>
2025-01-20 18:43:20 +0100

    fixed file upload

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,6 @@ const { pipeline } = require('node:stream/promises')
 fastify.register(require('@fastify/multipart'))
 
 fastify.post('/upload', async function (req, reply) {
-    // process a single file
-    // also, consider that if you allow to upload multiple files
-    // you must consume all files otherwise the promise will never fulfill
     const data = await req.file()
 
     data.file // stream
@@ -17,20 +14,9 @@ fastify.post('/upload', async function (req, reply) {
     data.encoding
     data.mimetype
 
-    // to accumulate the file in memory! Be careful!
-    //
-    // await data.toBuffer() // Buffer
-    //
-    // or
-
     await pipeline(data.file, fs.createWriteStream('backend/uploaded-file.txt'))
 
-    // be careful of permission issues on disk and not overwrite
-    // sensitive files that could cause security risks
-
-    // also, consider that if the file stream is not consumed, the promise will never fulfill
-
-    reply.send()
+    reply.send({ uploaded: true })
 })
 
 fastify.listen({ port: 3000 }, err => {
